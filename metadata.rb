d25c2930f368ac7319b2e095817eed8551d10beb
Noah Kobayashi <vohedge@gmail.com>
2014-11-13 19:14:33 +0900

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'wordpress'
+maintainer       'none'
+maintainer_email 'vohedge@gmail.com'
+license          'GPLv2'
+description      'Installs/Configures WordPress'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apache2'
+depends 'mysql'
+
